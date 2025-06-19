354f6f5fc94e8c1b0ccf91d9cf663ae9e92478ee
Codenboards <Codenboards@users.noreply.github.com>
2025-06-19 21:08:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const admin = require('firebase-admin');
 
 let serviceAccount;
 
-console.log("Raw env:", process.env.FIREBASE_SERVICE_ACCOUNT_JSON.slice(0, 100));
 
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 const app = express();
 const PORT = 4000;
@@ -22,26 +17,7 @@ app.get('/:targetDeviceToken', async (req, res) => {
 
   console.log(`Received request for device token: ${targetDeviceToken}`);
 
-  const message = {
-    notification: {
-      title: 'New order',
-      body: 'New order received, please check your dashboard!',
-    },
-    data: {
-      source: 'Qellner',
-      timestamp: new Date().toISOString(),
-    },
-    token: targetDeviceToken,
-  };
-
-  try {
-    const response = await admin.messaging().send(message);
-    console.log('Successfully sent message:', response);
-    res.status(200).json({ success: true, messageId: response, description: 'FCM message sent successfully.' });
-  } catch (error) {
-    console.error('Error sending message:', error);
-    res.status(500).json({ success: false, error: 'Failed to send FCM message.', details: error.message });
-  }
+ 
 });
 
 app.listen(PORT, () => {
